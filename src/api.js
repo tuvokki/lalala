f7ee40d498f9d360bb593a539ac2c83e0a1fc317
Wouter Roosendaal <tuvokki@gmail.com>
2017-09-27 15:06:39 +0200

    With api

---
+++ src/api.js
@@ -0,0 +1,17 @@
+import feathers from 'feathers'
+import auth from 'feathers-authentication-client'
+import hooks from 'feathers-hooks'
+import socketio from 'feathers-socketio'
+import io from 'socket.io-client'
+
+const socket = io('http://localhost:3030', { transports: ['websocket'] })
+
+const api = feathers()
+  .configure(hooks())
+  .configure(socketio(socket))
+  .configure(auth({ storage: window.localStorage }))
+
+api.service('/users')
+api.service('/games')
+
+export default api
--- src/components/Players.vue
+++ src/components/Players.vue
@@ -1,12 +1,70 @@
 <template>
   <div class="players">
     <h5>The players</h5>
+
+    <q-list highlight class="col-auto">
+      <q-list-header>People</q-list-header>
+      <q-item v-for="user in users" :key="user.id">
+        <q-item-side :avatar="user.avatar" />
+        <q-item-main>
+          <q-item-tile label>{{user.email}}</q-item-tile>
+        </q-item-main>
+        <q-item-side right>
+          <q-item-tile icon="chat_bubble" color="green" />
+        </q-item-side>
+      </q-item>
+    </q-list>
+
   </div>
 </template>
 
 <script>
+// import {
+//   QInput,
+//   QList,
+//   QListHeader,
+//   QItem,
+//   QItemTile,
+//   QItemMain,
+//   QItemSide,
+//   QChatMessage
+// } from 'quasar'
+import { Toast } from 'quasar'
+import api from 'src/api'
 export default {
-  name: 'players'
+  name: 'players',
+  props: ['user'],
+  data() {
+    return {
+      message: '',
+      messages: [],
+      users: []
+    }
+  },
+  computed: {
+  },
+  methods: {
+  },
+  mounted() {
+    const users = api.service('users')
+    // Authenticate
+    api.authenticate({
+      strategy: 'local',
+      email: 'wouter@tuvok.nl',
+      password: 'secret'
+    }).then(_ => {
+      Toast.create.positive('Authenticated')
+    })
+    // Get all users
+    users.find().then((response) => {
+      this.$data.users = response.data
+    })
+    // Listen to user events
+    users.on('created', user => {
+      this.$data.users = this.$data.users.concat(user)
+    })
+  },
+  beforeDestroy() { }
 }
 </script>
 
